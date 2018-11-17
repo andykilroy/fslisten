f72dfab75cb26f4bdf8ab2dd14d353262e7efd93
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2018-11-17 00:25:11 +0000

    arg parsing

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use std::time::Duration;
 use std::convert::AsRef;
 use std::path::Path;
 
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 fn watch<P: AsRef<Path>> (path: P) -> notify::Result<()> {
     let (tx, rx) = channel();
     let mut watcher: RecommendedWatcher = notify::watcher(tx, Duration::from_millis(100))?;
-    // let mut watcher: RecommendedWatcher = notify::raw_watcher(tx)?;
 
     watcher.watch(path, RecursiveMode::Recursive)?;
 
@@ -22,8 +24,19 @@ fn watch<P: AsRef<Path>> (path: P) -> notify::Result<()> {
 
 
 fn main() {
-    if let Err(e) = watch("/Users/akilroy/workspace/non-ts/rust/fslisten") {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Listen for operations on files and directories")
+        .arg(Arg::with_name("FILE")
+                      .help("The file or directory to monitor")
+                      .required(true)
+                      .index(1))
+        .get_matches();
+
+    let target = matches.value_of("FILE").expect("expected a file or directory on which to listen for changes");
+    if let Err(e) = watch(target) {
         eprintln!("error: {:?}", e)
     }
+
 }
 
