91c84f294c77bfcb9c8195339f1e7d56ffb48d37
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2018-11-17 00:05:53 +0000

    debounced events

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,28 @@ extern crate notify;
 use notify::{RecommendedWatcher, Watcher, RecursiveMode};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use std::convert::AsRef;
+use std::path::Path;
 
-fn watch() -> notify::Result<()> {
+fn watch<P: AsRef<Path>> (path: P) -> notify::Result<()> {
     let (tx, rx) = channel();
-    // let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2))?;
-    let mut watcher: RecommendedWatcher = notify::raw_watcher(tx)?;
+    let mut watcher: RecommendedWatcher = notify::watcher(tx, Duration::from_millis(100))?;
+    // let mut watcher: RecommendedWatcher = notify::raw_watcher(tx)?;
 
-    watcher.watch("/Users/akilroy/workspace/non-ts/rust/fslisten", RecursiveMode::Recursive)?;
+    watcher.watch(path, RecursiveMode::Recursive)?;
 
     loop {
         match rx.recv() {
             Ok(event) => println!("{:?}", event),
-            Err(e) => println!("watch error: {:?}", e),
+            Err(e) => eprintln!("watch error: {:?}", e),
         }
     }
 }
 
+
 fn main() {
-    if let Err(e) = watch() {
-        println!("error: {:?}", e)
+    if let Err(e) = watch("/Users/akilroy/workspace/non-ts/rust/fslisten") {
+        eprintln!("error: {:?}", e)
     }
 }
 
