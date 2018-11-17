84937a8614d27ae4f4bbd787044ead1e26c3c964
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2018-11-17 00:51:14 +0000

    format printouts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate notify;
-use notify::{RecommendedWatcher, Watcher, RecursiveMode};
+use notify::{RecommendedWatcher, Watcher, RecursiveMode, DebouncedEvent};
+use notify::DebouncedEvent::*;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use std::convert::AsRef;
@@ -16,9 +17,23 @@ fn watch<P: AsRef<Path>> (path: P) -> notify::Result<()> {
 
     loop {
         match rx.recv() {
-            Ok(event) => println!("{:?}", event),
+            Ok(ev) => print_debounced(ev),
             Err(e) => eprintln!("watch error: {:?}", e),
         }
+    }
+}
+
+fn print_debounced(event: DebouncedEvent) {
+    match event {
+        NoticeWrite(p)  => println!("write(1) {}", p.to_str().unwrap_or("")),
+        NoticeRemove(p) => println!("rm(1)    {}", p.to_str().unwrap_or("")),
+        Create(p)       => println!("create   {}", p.to_str().unwrap_or("")),
+        Write(p)        => println!("write    {}", p.to_str().unwrap_or("")),
+        Chmod(p)        => println!("chmod    {}", p.to_str().unwrap_or("")),
+        Remove(p)       => println!("rm       {}", p.to_str().unwrap_or("")),
+        Rename(p, q)    => println!("mv       {} {}", p.to_str().unwrap_or(""), q.to_str().unwrap_or("")),
+        Rescan          => println!("rescan"),
+        Error(err, opt) => println!("error    {}, {:?}", err, opt)
     }
 }
 
